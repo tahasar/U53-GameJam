7997e0831287b80b1580337702af72c1954b698e
Hüseyin Yıldırım <husoyldrmm@outlook.com>
2023-04-21 00:44:13 +0300

    son

--- Assets/GAMEJAM/Scripts/KeyBoardController.cs
+++ Assets/GAMEJAM/Scripts/KeyBoardController.cs
@@ -404,6 +404,10 @@ public class KeyBoardController : MonoBehaviour
         newEmissionIntensity = currentEmission.maxColorComponent + 20f;
         Color newEmissionColor = currentEmission * (newEmissionIntensity / currentEmission.maxColorComponent);
         escMat.SetColor("_EmissionColor", newEmissionColor);
+<<<<<<< Updated upstream
+=======
+        Debug.Log("xd");
+>>>>>>> Stashed changes
 
         yield return new WaitForSeconds(time);
         currentEmission = escMat.GetColor("_EmissionColor");
